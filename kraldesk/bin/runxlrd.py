dae693eedd543b5d8760e2d1701848fe5a51837a
CapPow <calebadampowell@gmail.com>
2017-11-11 10:49:08 -0500

    clean up
    cleaning excessive files.

--- kraldesk/bin/runxlrd.py
+++
@@ -1,4 +0,0 @@
-#!/home/jacob/Programming/Projects/Plant-Digitization-Project/kraldesk/bin/python3
-# EASY-INSTALL-SCRIPT: 'xlrd==1.1.0','runxlrd.py'
-__requires__ = 'xlrd==1.1.0'
-__import__('pkg_resources').run_script('xlrd==1.1.0', 'runxlrd.py')
