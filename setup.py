b5ca8c0a607bb00ccbaaaeff4b761b6d79adb106
CapPow <calebadampowell@gmail.com>
2017-11-12 07:59:39 -0500

    setup

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='kraldesk',
-    version='0.1dev',
-    license='GPL v3',
-    packages=find_packages(),
-    data_files=[('kraldesk',['sample-data/*.csv'])]
-)
