6a374578801e206cb07e1e81a01bc1cd2da3fd2d
CapPow <calebadampowell@gmail.com>
2017-11-12 08:13:11 -0500

    setup

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ setup(
     version = '0.1dev',
     description = '',
     license='GPL v3',
-    packages = [''],
     package_data={'': ['sample-data/*.csv']}
     install_requires=['pandas>=0.18']
 )
