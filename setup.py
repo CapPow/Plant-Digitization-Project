8b79b9f3aa18c5b55b5f38666b746012d25a9cba
CapPow <calebadampowell@gmail.com>
2017-11-12 08:05:31 -0500

    setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+import sys,os
+home=os.path.expanduser('~')
+
+setup(
+    name = 'kraldesk',
+    version = '0.1dev',
+    description = '',
+    license='GPL v3',
+    packages = [''],
+    package_data={'': ['sample-data/*.csv',
+    install_requires=['pandas>=0.18']
+)
