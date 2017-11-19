5a32a1e7d05cd84d0a51333c4da7e07342a5adf6
CapPow <calebadampowell@gmail.com>
2017-11-19 17:33:48 -0500

    improved CoL Error handling
    Added alternative CoL Query attempts when services is down.

--- kraldesk/catalogOfLife.py
+++ kraldesk/catalogOfLife.py
@@ -8,6 +8,7 @@ import re
 import xml.etree.ElementTree as ET
 import html
 import sys
+import datetime
 
 
 # catalog of life scientific name search
@@ -31,7 +32,17 @@ def colNameSearch(givenScientificName):
     try:
         CoLQuery = ET.parse(urllib.request.urlopen('http://webservice.catalogueoflife.org/col/webservice?name={}&response=terse'.format('+'.join(identQuery)), timeout=30)).getroot()
     except HTTPError:
-        return 'http_Error'
+        try:
+            #This try attempt tries to load a catalog by specififying the current year
+            #We may want to ask the user first, or consider removing this.
+            #This tries  the current year then attempts a year prior before giving up.
+            print('useing the alternative CoL URL')
+            CoLQuery = ET.parse(urllib.request.urlopen('http://webservice.catalogueoflife.org/annual-checklist/{}/webservice?name={}&response=terse'.format(datetime.datetime.now().year,'+'.join(identQuery)), timeout=30)).getroot()
+        except HTTPError:
+            try:
+                CoLQuery = ET.parse(urllib.request.urlopen('http://webservice.catalogueoflife.org/annual-checklist/{}/webservice?name={}&response=terse'.format(datetime.datetime.now().year -1 ,'+'.join(identQuery)), timeout=30)).getroot()
+            except HTTPError:
+                return 'http_Error'
     #<status>accepted name|ambiguous synonym|misapplied name|privisionally acceptedname|synomym</status>  List of potential name status
 
     for result in CoLQuery.findall('result'):
