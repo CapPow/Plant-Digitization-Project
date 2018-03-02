3f2eb8dca1462bd9b7ba791bbb9565c9d0b4a8cd
Caleb Powell <calebadampowell@gmail.com>
2018-03-02 11:34:27 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     version = '0.1dev',
     description = '',
     license='GPL v3',
-    packages=['pdproject'],
+    package_dir = {'': 'src'}
     package_data={'kraldesk': ['sample-data/*.csv']},
     install_requires=['reportlab','pandas','numpy']
 )
